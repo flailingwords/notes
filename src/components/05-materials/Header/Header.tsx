0e67881504073b17edd232fe01132b64c290a200
Nick Harrow <nick.harrow.me@gmail.com>
2025-12-07 16:32:04 -0800

    feat: improved Header handling and colours

--- src/components/05-materials/Header/Header.tsx
+++ src/components/05-materials/Header/Header.tsx
@@ -8,7 +8,7 @@ import RepoIconLink from '@/components/02-molecules/RepoIconLink/RepoIconLink'
 import ThemeSelector from '@/components/03-particles/ThemeSelector/ThemeSelector'
 
 const Header: FC<HeaderProps> = () => (
-    <div className='shadow-nav-light dark:shadow-nav-dark fixed top-0 right-0 left-0 z-10 grid h-10 grid-cols-2 *:w-fit *:min-w-32 *:p-1.5 *:align-middle'>
+    <div className='shadow-nav-light dark:shadow-nav-dark dark:bg-dark-bg bg-light-bg fixed top-0 right-0 left-0 z-10 grid h-10 grid-cols-2 opacity-100 *:w-fit *:min-w-32 *:p-1.5 *:align-middle'>
         <div className='justify-self-start'>
             <Link to={import.meta.env.BASE_URL}>Nick&apos;s Notes</Link>
         </div>
